3149f612b2d837f03a21c8d24bb0c4bfcb63ca72
Paras Gupta <parasgupta4494@gmail.com>
2026-01-01 15:38:53 +0530

    done

--- reset_today.sql
+++
@@ -1,6 +0,0 @@
--- RESET ATTENDANCE FOR 01/01/2026
--- This will delete all sessions started on this day.
--- Because of 'ON DELETE CASCADE', all related attendance_records (Present/Absent) will also be deleted.
-
-DELETE FROM attendance_sessions
-WHERE start_time::date = '2026-01-01';
